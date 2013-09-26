bcfe5044e9f43c1cf115641bf6d86035d7fb75f0
Guo Yu <i@guoyu.me>
2013-09-26 23:00:30 +0800

    fixed demo.jpg url

--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,5 @@
 var Ascii = require('../index');
-var pic = new Ascii(__dirname + '/yr.jpg');
+var pic = new Ascii(__dirname + '/demo.jpg');
 var fs = require('fs');
 
 pic.convert(function(err, result) {
