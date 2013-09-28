f533961071cc00ebd5f7de70a848de01928a84cb
Guo Yu <i@guoyu.me>
2013-09-29 00:01:51 +0800

    delete comments

--- demo/index.js
+++ demo/index.js
@@ -4,6 +4,5 @@ var fs = require('fs');
 
 pic.convert(function(err, result) {
     console.log(result);
-    // var style = '<style> * {margin: 0;padding: 0;} body {font-size: 12px; margin: 10px; font-family: simsun; background: #fff;} p { height: 12px;}</style>';
     // fs.writeFile('demo.html', result);
 });
