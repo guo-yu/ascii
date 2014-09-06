205fd9fe182629f90b6f42e37ee9a0bb9f224824
Guo Yu <i@guoyu.me>
2014-09-06 15:29:50 +0700

    0.1: improve example

--- example/index.js
+++ example/index.js
@@ -1,8 +1,16 @@
 var Ascii = require('../index');
-var pic = new Ascii(__dirname + '/demo2.jpg');
+var pic1 = new Ascii(__dirname + '/demo.jpg');
+var pic2 = new Ascii(__dirname + '/demo2.jpg');
 var fs = require('fs');
 
-pic.convert(function(err, result) {
+pic1.convert(function(err, result) {
+  console.log(result);
+  // fs.writeFile('demo.html', result);
+});
+
+setTimeout(function(){
+  pic2.convert(function(err, result) {
     console.log(result);
     // fs.writeFile('demo.html', result);
-});
+  });  
+}, 1000);
